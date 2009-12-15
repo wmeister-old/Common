db45a58ae5dd8bfb6fc54f12dc07f2b246c84e9a
Billy <billy@billy-desktop.(none)>
2009-12-15 16:52:58 -0500

    fixed bug in indexOf

--- Array.pm
+++ Array.pm
@@ -6,7 +6,7 @@ our @EXPORT_OK = qw(indexOf);
 
 sub indexOf {
     my $m = shift;
-    for(my $i = 0; $i <= scalar @_; $i++) { return $i if $m eq $_[$i]; }
+    for(my $i = 0; $i <= (scalar(@_) - 1); $i++) { return $i if $m eq $_[$i]; }
     return undef;
 }
 
